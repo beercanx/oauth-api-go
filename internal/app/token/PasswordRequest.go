ea3457cdec8ecb9136eb70681683c8ca4deb1335
James Bacon <james@baconi.co.uk>
2025-02-22 16:56:14 +0000

    Removed uncesseary json tags from struct.

--- internal/app/token/PasswordRequest.go
+++ internal/app/token/PasswordRequest.go
@@ -2,11 +2,11 @@ package token
 
 import "baconi.co.uk/oauth/internal/pkg/client"
 
-type PasswordRequest struct { // TODO - Remove json tags its only for debug during build
-	Principal client.Principal `json:"principal"` // TODO - Work out how we can change this to only return say client.Id
-	Scopes    []string         `json:"scopes,omitempty"`
-	Username  string           `json:"username,omitempty"`
-	Password  string           `json:"password,omitempty"`
+type PasswordRequest struct {
+	Principal client.Principal
+	Scopes    []string
+	Username  string
+	Password  string
 }
 
 func (request PasswordRequest) getPrincipal() client.Principal {
