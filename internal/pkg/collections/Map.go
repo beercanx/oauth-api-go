3d0512c20e2afb85ea72c9b0c2bf23fb22d4e5ac
James Bacon <james@baconi.co.uk>
2025-06-19 10:41:30 +0100

    Removed collection functions
    Feels like these concepts might go against basic Go principles
    
    Also generated by AI so no real brain time waisted on writing them.

--- internal/pkg/collections/Map.go
+++
@@ -1,9 +0,0 @@
-package collections
-
-func Map[T, R any](array []T, transform func(T) R) []R {
-	results := make([]R, len(array))
-	for index, item := range array {
-		results[index] = transform(item)
-	}
-	return results
-}
