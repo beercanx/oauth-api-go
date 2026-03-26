490d453f9a41e8984f3e89d9f2d1fa194fc8a6c1
James Bacon <james@baconi.co.uk>
2026-03-26 16:23:29 +0000

    More integration tests covering introspection http route

--- internal/app/token-introspection/Route_integration_test.go
+++ internal/app/token-introspection/Route_integration_test.go
@@ -2,13 +2,18 @@
 package token_introspection
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 
 	"baconi.co.uk/oauth/internal/pkg/client"
+	"baconi.co.uk/oauth/internal/pkg/scope"
 	"baconi.co.uk/oauth/internal/pkg/token"
+	"baconi.co.uk/oauth/internal/pkg/user"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
@@ -30,41 +35,208 @@ func TestTokenIntrospectionRequests(t *testing.T) {
 
 	Route(router, clientAuthenticationService, tokenIntrospector)
 
-	for _, invalidMethod := range []string{
-		http.MethodGet,
-		http.MethodDelete,
-		http.MethodConnect,
-		http.MethodHead,
-		http.MethodOptions,
-		http.MethodPatch,
-		http.MethodPut,
-		http.MethodTrace,
-	} {
-		t.Run(fmt.Sprintf("should allow only post requests %s", invalidMethod), func(t *testing.T) {
+	t.Run("should allow only post requests", func(t *testing.T) {
+
+		for _, invalidMethod := range []string{
+			http.MethodGet,
+			http.MethodDelete,
+			http.MethodConnect,
+			http.MethodHead,
+			http.MethodOptions,
+			http.MethodPatch,
+			http.MethodPut,
+			http.MethodTrace,
+		} {
+			t.Run(fmt.Sprintf("reject %s", invalidMethod), func(t *testing.T) {
+
+				testRequest := httptest.NewRequest(invalidMethod, "/introspect", nil)
+
+				recorder := httptest.NewRecorder()
+
+				router.ServeHTTP(recorder, testRequest)
+
+				assert.Equal(t, http.StatusMethodNotAllowed, recorder.Code)
+				assert.NotEmpty(t, recorder.Header())
+				assert.Equal(t, http.MethodPost, recorder.Header().Get("Allow"))
+				assert.Zero(t, recorder.Header().Get("WWW-Authenticate"))
+			})
+		}
+	})
+
+	t.Run("must allow only authorized requests", func(t *testing.T) {
+
+		t.Run("reject missing authentication", func(t *testing.T) {
+
+			testRequest := httptest.NewRequest(http.MethodPost, "/introspect", nil)
 
 			recorder := httptest.NewRecorder()
-			request := httptest.NewRequest(invalidMethod, "/introspect", nil)
 
-			router.ServeHTTP(recorder, request)
+			router.ServeHTTP(recorder, testRequest)
 
-			assert.Equal(t, http.StatusMethodNotAllowed, recorder.Code)
-			assert.NotEmpty(t, recorder.Header())
-			assert.Equal(t, http.MethodPost, recorder.Header().Get("Allow"))
-			assert.Zero(t, recorder.Header().Get("WWW-Authenticate"))
+			assert.Equal(t, http.StatusUnauthorized, recorder.Code)
+			assert.Empty(t, recorder.Header())
+			assert.Empty(t, recorder.Body.String())
 		})
-	}
 
-	// must allow only authorised requests
-	// - reject missing authentication
-	// - reject invalid basic authentication
-	// - reject public client authentication
-	// - reject a valid client that is missing the introspection allowed action
-	// - accept a valid client using basic authentication
+		t.Run("reject invalid basic authentication", func(t *testing.T) {
 
-	// should allow only url encoded form requests
-	// - reject JSON body requests
-	// - reject XML body requests
-	// - reject non url encoded form posts
-	// - accept URL encoded form body requests
+			testRequest := httptest.NewRequest(http.MethodPost, "/introspect", nil)
+			testRequest.SetBasicAuth("invalid", "invalid")
 
+			recorder := httptest.NewRecorder()
+
+			router.ServeHTTP(recorder, testRequest)
+
+			assert.Equal(t, http.StatusUnauthorized, recorder.Code)
+			assert.Empty(t, recorder.Header())
+			assert.Empty(t, recorder.Body.String())
+		})
+
+		t.Run("reject public client authentication", func(t *testing.T) {
+
+			formBody := url.Values{"client_id": {"cicada"}, "token": {"a"}}
+			testRequest := httptest.NewRequest(http.MethodPost, "/introspect", strings.NewReader(formBody.Encode()))
+			testRequest.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+			recorder := httptest.NewRecorder()
+
+			router.ServeHTTP(recorder, testRequest)
+
+			assert.Equal(t, http.StatusUnauthorized, recorder.Code)
+			assert.Empty(t, recorder.Header())
+			assert.Empty(t, recorder.Body.String())
+		})
+
+		t.Run("reject a valid client missing the introspection allowed action", func(t *testing.T) {
+
+			formBody := url.Values{"token": {"a"}}
+			testRequest := httptest.NewRequest(http.MethodPost, "/introspect", strings.NewReader(formBody.Encode()))
+			testRequest.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			testRequest.SetBasicAuth("dodo", "echidna")
+
+			recorder := httptest.NewRecorder()
+
+			router.ServeHTTP(recorder, testRequest)
+
+			assert.Equal(t, http.StatusForbidden, recorder.Code)
+			assert.NotEmpty(t, recorder.Header())
+			assert.Equal(t, "application/json; charset=utf-8", recorder.Header().Get("Content-Type"))
+
+			var result invalid
+			err := json.Unmarshal(recorder.Body.Bytes(), &result)
+			assert.Nil(t, err)
+			assert.Equal(t, UnauthorizedClient, result.ErrorType)
+			assert.Equal(t, "client is not allowed to introspect", result.Description)
+		})
+
+		// TODO - Will be replaced by TokenState::missingToken
+		t.Run("accept a valid client using basic authentication", func(t *testing.T) {
+
+			formBody := url.Values{"token": {"94efe4d7-7dbe-455f-b974-46656fd8d035"}}
+			testRequest := httptest.NewRequest(http.MethodPost, "/introspect", strings.NewReader(formBody.Encode()))
+			testRequest.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			testRequest.SetBasicAuth("aardvark", "badger")
+
+			recorder := httptest.NewRecorder()
+
+			router.ServeHTTP(recorder, testRequest)
+
+			assert.Equal(t, http.StatusOK, recorder.Code)
+			assert.Equal(t, `{"active":false}`, recorder.Body.String())
+		})
+	})
+
+	t.Run("should allow only url encoded form requests", func(t *testing.T) {
+
+		t.Run("reject JSON body requests", func(t *testing.T) {
+
+			jsonBody := `{"token":"94efe4d7-7dbe-455f-b974-46656fd8d035"}`
+			testRequest := httptest.NewRequest(http.MethodPost, "/introspect", strings.NewReader(jsonBody))
+			testRequest.Header.Set("Content-Type", "application/json")
+			testRequest.SetBasicAuth("aardvark", "badger")
+
+			recorder := httptest.NewRecorder()
+
+			router.ServeHTTP(recorder, testRequest)
+
+			assert.Equal(t, http.StatusUnsupportedMediaType, recorder.Code)
+			assert.Equal(t, `{"error":"invalid_request","description":"Content-Type must be application/x-www-form-urlencoded"}`, recorder.Body.String())
+		})
+
+		t.Run("reject XML body requests", func(t *testing.T) {
+
+			xmlBody := `<request><token>94efe4d7-7dbe-455f-b974-46656fd8d035</token></request>`
+			testRequest := httptest.NewRequest(http.MethodPost, "/introspect", strings.NewReader(xmlBody))
+			testRequest.Header.Set("Content-Type", "application/xml")
+			testRequest.SetBasicAuth("aardvark", "badger")
+
+			recorder := httptest.NewRecorder()
+
+			router.ServeHTTP(recorder, testRequest)
+
+			assert.Equal(t, http.StatusUnsupportedMediaType, recorder.Code)
+			assert.Equal(t, `{"error":"invalid_request","description":"Content-Type must be application/x-www-form-urlencoded"}`, recorder.Body.String())
+		})
+
+		t.Run("reject non url encoded form posts", func(t *testing.T) {
+
+			formBody := url.Values{"token": {"a"}}
+			testRequest := httptest.NewRequest(http.MethodPost, "/introspect", strings.NewReader(formBody.Encode()))
+			testRequest.SetBasicAuth("aardvark", "badger")
+
+			recorder := httptest.NewRecorder()
+
+			router.ServeHTTP(recorder, testRequest)
+
+			assert.Equal(t, http.StatusUnsupportedMediaType, recorder.Code)
+			assert.Equal(t, `{"error":"invalid_request","description":"Content-Type must be application/x-www-form-urlencoded"}`, recorder.Body.String())
+		})
+
+		// TODO - Will be replaced by TokenState::activeToken
+		t.Run("accept URL encoded form body requests", func(t *testing.T) {
+
+			accessTokenIssuer := token.NewAccessTokenIssuer(accessTokenRepository)
+			username := user.AuthenticatedUsername{Value: "ant"}
+			clientId := client.Id{Value: "dodo"}
+			accessToken, issueError := accessTokenIssuer.Issue(username, clientId, scope.Scopes{})
+			assert.Nil(t, issueError)
+
+			formBody := url.Values{"token": {accessToken.Value.String()}}
+			testRequest := httptest.NewRequest(http.MethodPost, "/introspect", strings.NewReader(formBody.Encode()))
+			testRequest.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			testRequest.SetBasicAuth("aardvark", "badger")
+
+			recorder := httptest.NewRecorder()
+
+			router.ServeHTTP(recorder, testRequest)
+
+			assert.Equal(t, http.StatusOK, recorder.Code)
+			assert.NotEmpty(t, recorder.Header())
+			assert.Equal(t, "application/json; charset=utf-8", recorder.Header().Get("Content-Type"))
+
+			var result map[string]any
+			unmarshalError := json.Unmarshal(recorder.Body.Bytes(), &result)
+			assert.Nil(t, unmarshalError)
+			assert.Equal(t, true, result["active"])
+			assert.Equal(t, "dodo", result["client_id"])
+			assert.Contains(t, result, "expiration_time")
+			assert.Contains(t, result, "issued_at")
+			assert.Contains(t, result, "not_before")
+			assert.Equal(t, "ant", result["sub"])
+			assert.Equal(t, "bearer", result["token_type"])
+			assert.Equal(t, "ant", result["username"])
+			assert.Len(t, result, 8)
+		})
+	})
+
+	// TODO: InvalidBodyContent
+	// 	- missing token
+	// 	- blank token
+	// 	- non uuid token
+
+	// TODO: TokenStates
+	// 	- missing token
+	// 	- expired token
+	// 	- future token
+	// 	- active token
 }
