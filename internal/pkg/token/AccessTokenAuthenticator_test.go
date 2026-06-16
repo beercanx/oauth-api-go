dca8af0e94048ecfeb0e2981bd749a20d7bb3907
James Bacon <james@baconi.co.uk>
2026-06-16 10:47:43 +0100

    AccessToken authentiactor coverage

---
+++ internal/pkg/token/AccessTokenAuthenticator_test.go
@@ -0,0 +1,86 @@
+package token
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+var testErrorNoDatabase = errors.New("no database")
+
+func TestAccessTokenAuthenticator_Authenticate(t *testing.T) {
+	t.Parallel()
+
+	t.Run("should return error is repository returns error", func(t *testing.T) {
+		t.Parallel()
+
+		repository := NewMockRepositoryReadDelete[AccessToken](t)
+		underTest := NewAccessTokenAuthenticator(repository)
+
+		repository.EXPECT().FindById(mock.AnythingOfType("uuid.UUID")).Return(AccessToken{}, testErrorNoDatabase).Once()
+
+		token, err := underTest.Authenticate(uuid.New())
+
+		require.ErrorIs(t, err, testErrorNoDatabase)
+		assert.Zero(t, token)
+	})
+
+	t.Run("should return error if access token is expired", func(t *testing.T) {
+		t.Parallel()
+
+		repository := NewMockRepositoryReadDelete[AccessToken](t)
+		underTest := NewAccessTokenAuthenticator(repository)
+
+		token := uuid.New()
+		dbToken := AccessToken{ID: token, ExpiresAt: time.Now().Add(-time.Hour)}
+
+		repository.EXPECT().FindById(token).Return(dbToken, nil).Once()
+		repository.EXPECT().DeleteByRecord(dbToken).Return(nil).Once()
+
+		accessToken, err := underTest.Authenticate(token)
+
+		require.ErrorIs(t, err, ErrTokenHasExpired)
+		assert.Zero(t, accessToken)
+		assert.NotEqual(t, dbToken, accessToken)
+	})
+
+	t.Run("should return error if access token is before now", func(t *testing.T) {
+		t.Parallel()
+
+		repository := NewMockRepositoryReadDelete[AccessToken](t)
+		underTest := NewAccessTokenAuthenticator(repository)
+
+		token := uuid.New()
+		dbToken := AccessToken{ID: token, ExpiresAt: time.Now().Add(time.Hour), NotBefore: time.Now().Add(time.Minute)}
+
+		repository.EXPECT().FindById(token).Return(dbToken, nil).Once()
+
+		accessToken, err := underTest.Authenticate(token)
+
+		require.ErrorIs(t, err, ErrTokenIsBefore)
+		assert.Zero(t, accessToken)
+		assert.NotEqual(t, dbToken, accessToken)
+	})
+
+	t.Run("should return access token if it is valid", func(t *testing.T) {
+		t.Parallel()
+
+		repository := NewMockRepositoryReadDelete[AccessToken](t)
+		underTest := NewAccessTokenAuthenticator(repository)
+
+		token := uuid.New()
+		dbToken := AccessToken{ID: token, ExpiresAt: time.Now().Add(time.Hour), NotBefore: time.Now().Add(-time.Minute)}
+
+		repository.EXPECT().FindById(token).Return(dbToken, nil).Once()
+
+		accessToken, err := underTest.Authenticate(token)
+
+		require.NoError(t, err)
+		assert.Equal(t, dbToken, accessToken)
+	})
+}
